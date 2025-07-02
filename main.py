4d2477f33f5ec28033dd60b1fcac6fd0598556b6
Chase C. <rudrapatel26122004@gmail.com>
2025-07-02 11:06:28 -0400

    fixed ref import

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from shadow_db import Novel, Chapter  # Adjusted import to match the new structure
 from vllm.connector import translate_chapter
-from utils.context import save_translated_chapter
+from utils.refrences import save_translated_chapter
 
 def main():
     chapter = Chapter.get(novel_id=1, chapter_number=1)
